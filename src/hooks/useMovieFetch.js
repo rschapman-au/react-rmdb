791112556cb96cc623ae50756736cde85f8c2e9c
Ryan Chapman <rschapman.au@gmail.com>
2021-11-05 21:20:36 +1100

    First commit for react movie database project

---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,40 @@
+import userEvent from "@testing-library/user-event";
+import { useState, useEffect } from "react";
+import { useRoutes } from "react-router";
+import API from "../API";
+
+export const useMovieFetch = movieId => {
+    const [state, setState] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+                setLoading(true);
+                setError(false);
+
+                const movie = await API.fetchMovie(movieId);
+                const credits = await API.fetchCredits(movieId);
+                // Get directors only
+                const directors = credits.crew.filter(
+                    member => member.job === 'Director'
+                );
+
+                setState({
+                    ...movie, 
+                    actors: credits.cast,
+                    directors: directors
+                })
+
+                setLoading(false);
+            } catch (error) {
+                setError(true);
+            }
+
+        }
+        fetchMovie();
+    }, [movieId])
+
+    return { state, loading, error };
+}
