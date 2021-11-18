2f13cc516890ded06194745ffa747439c7c10131
Ryan Chapman <rschapman.au@gmail.com>
2021-11-18 23:08:34 +1100

    Created Rate Component

---
+++ src/components/Rate/Rate.styles.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+`;
+
+export const Content = styled.div`
+    width: 100%auto;
+    padding: 20px 40px;
+    overflow: hidden;
+
+    .slider-score {
+        display: flex;
+        justify-content: flex-start;
+    }
+        
+    .slider {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        input[type="range"] {
+        outline: 0;
+        border: 0;
+        border-radius: 250px;
+        width: 350px;
+        max-width: 100%;
+        margin: 24px 0 16px;
+        transition: box-shadow 0.2s ease-in-out;
+        background-color: var(--darkGrey);
+        height: 30px;
+        -webkit-appearance: none;
+        
+
+        ::-webkit-slider-runnable-track {
+                height: 30px;
+                -webkit-appearance: none;
+                color: var(--medGrey);
+                transition: box-shadow 0.2s ease-in-out;     
+            }
+        ::-webkit-slider-thumb {
+                width: 30px;
+                -webkit-appearance: none;
+                height: 30px;
+                cursor: pointer;
+                background: var(--lightGrey);
+                border-radius: 50%;
+                transition: box-shadow 0.2s ease-in-out;
+                position: relative;
+                // top: 1px;
+            }
+            :active::-webkit-slider-thumb {
+                background: var(--lightGrey);
+                box-shadow: 0px 0 0 2px lightskyblue;
+                //box-shadow: -340px 0 0 320px #1597ff, inset 0 0 0 3px #1597ff;
+            }
+    }
+    }
+
+    .rating-score {
+        margin: 0 0 0 0px;
+        align-items: center;
+        justify-content: center;
+
+        p {
+            margin: 0;
+        }
+    }
+`;
---
+++ src/components/Rate/index.js
@@ -0,0 +1,54 @@
+import React, {useState, useContext} from "react";
+import { Wrapper, Content } from "./Rate.styles";
+import Button  from "../Button"
+import API from "../../API"
+
+//Context
+import { Context } from "../../context";
+
+//Types
+//Types
+import { MovieState } from "../../hooks/useMovieFetch";
+
+
+
+
+const Rate = ({ movie }) => {
+    const [value, setValue] = useState(5);
+    const [user] = useContext(Context);
+
+    const handleRating = async (value) => {
+        const rate = await API.rateMovie(user.sessionId, movie.id, value)
+        console.log(rate)
+    }
+
+    return (
+        <Wrapper>
+            <Content>
+                <div className = 'slider-score'>
+                    <div className = 'slider'>
+                    <input 
+                        type='range'
+                        min='1'
+                        max='10'
+                        value={value}
+                        onChange={e => setValue(Number(e.currentTarget.value))}
+                    /> 
+
+                    </div>
+                    <div className ='rating-score'>
+                        <p>{value}</p>
+                    </div>
+                 
+                </div>
+                
+                <div>
+                    <Button text='Rate' callback = {()=>handleRating(value)}/>
+                </div>
+                
+            </Content>
+        </Wrapper>
+    )
+}
+
+export default Rate;
