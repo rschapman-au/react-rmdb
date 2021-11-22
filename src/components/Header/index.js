406e7d6aab7dc8e8c63b48e13ec07bc7183833c5
Ryan Chapman <rschapman.au@gmail.com>
2021-11-22 21:41:02 +1100

    fixed deploy issue

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,8 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import RMDBLogo from "../../images/react-movie-logo.svg"
-import TMDBLogo from "../../images/tmdb_logo.svg"
 
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import { Wrapper, Content, LogoImg } from "./Header.styles";
 
 //Context
 import { Context } from "../../context";
